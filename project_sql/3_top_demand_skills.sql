0393eb743fbd7655581235e946e54b345189a31b
timinath <natholuwatimileyin@gmail.com>
2024-04-07 17:12:32 +0100

    upload

--- project_sql/3_top_demand_skills.sql
+++ project_sql/3_top_demand_skills.sql
@@ -15,7 +15,7 @@ JOIN skills_job_dim
   ON job_postings_fact.job_id = skills_job_dim.job_id
 JOIN skills_dim
   ON skills_job_dim.skill_id = skills_dim.skill_id
-WHERE job_title_short = 'Data Analyst'
+WHERE job_title_short = 'Data Analyst' AND job_work_from_home = True
 AND job_work_from_home = True
 GROUP BY skills
 ORDER BY number_demanded DESC
